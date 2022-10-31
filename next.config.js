f25813c4b6453322b21c89035292b9e3ff7dac2a
togongs <jcw9828@gmail.com>
2022-10-31 17:35:10 +0900

    fix next export error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
   swcMinify: true,
   reactStrictMode: false,
   trailingSlash: true,
